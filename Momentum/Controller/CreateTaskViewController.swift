b916932078ceec4bcc6de76e77ab9a5b266c8bd0
SemaDevpy <syimykdev@gmail.com>
2021-04-02 20:30:48 +0600

    unknown commit

---
+++ Momentum/Controller/CreateTaskViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CreateTaskViewController.swift
+//  Momentum
+//
+//  Created by Syimyk on 4/2/21.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
